a9ee292247a6d864b84f43b83aa1e145c7a3c069
Liedson Correia <liedsoncanal@gmail.com>
2022-09-10 19:05:40 -0100

    chore: create data base

---
+++ lib/data.ts
@@ -0,0 +1,15 @@
+const data = [
+    {
+        id: "logoImg",
+        img: "../src/assets/logo.png",
+    },
+    {
+        id: "mainImg",
+        img: "../src/assets/mainImg.png",
+    },
+    {
+        id: "whoAmI",
+        title: "Who am I",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at pellentesque risus. Phasellus auctor tortor a ligula tempus, quis vulputate lorem pulvinar. Duis elementum eleifend ultricies. Vestibulum efficitur tempor dolor."
+    }
+];
