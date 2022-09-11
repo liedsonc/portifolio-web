fd228e460c5bd3b9e666e8d8b5cdcd26340f8ce2
Liedson Correia <liedsoncanal@gmail.com>
2022-09-11 19:04:57 -0100

    chore: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { resetCss } from "./config/stitches.config";
+import Home from './pages/Home'
 resetCss();
 function App() {
   return (
     <div className="App">
-
+      <Home></Home>
     </div>
   )
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import './style.css'
+import { Navbar } from '../../components/Navbar';
+
+
+function Home() {
+    return (
+        <div>
+            <Navbar>
+                <h1>Navbar</h1>
+
+            </Navbar>
+
+        </div >
+    );
+}
+
+export default Home;
