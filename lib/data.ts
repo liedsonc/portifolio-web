75eb9a32325beab03b9fc708bc0109d1afa2652a
Liedson Correia <liedsoncanal@gmail.com>
2022-09-25 02:16:28 -0100

    chore: create data file

--- lib/data.ts
+++ lib/data.ts
@@ -1,15 +1,12 @@
-const data = [
-    {
-        id: "logoImg",
-        img: "../src/assets/logo.png",
-    },
-    {
-        id: "mainImg",
-        img: "../src/assets/mainImg.png",
-    },
-    {
-        id: "whoAmI",
-        title: "Who am I",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at pellentesque risus. Phasellus auctor tortor a ligula tempus, quis vulputate lorem pulvinar. Duis elementum eleifend ultricies. Vestibulum efficitur tempor dolor."
-    }
-];
+export const logo = {
+    img: "/assets/logo.png",
+}
+
+export const main = {
+    img: "/assets/mainImg.png",
+}
+
+export const WhoAmI = {
+    title: "Who am I",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at pellentesque risus. Phasellus auctor tortor a ligula tempus, quis vulputate lorem pulvinar. Duis elementum eleifend ultricies. Vestibulum efficitur tempor dolor."
+}
