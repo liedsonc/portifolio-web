5c751ecdc8afcf95d7c9cdfcb3ba0c374d7e5a6d
Liedson Correia <liedsoncanal@gmail.com>
2022-10-02 19:25:16 -0100

    chore: add technologies

--- lib/data.ts
+++ lib/data.ts
@@ -10,3 +10,26 @@ export const WhoAmI = {
     title: "Who am I",
     content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at pellentesque risus. Phasellus auctor tortor a ligula tempus, quis vulputate lorem pulvinar. Duis elementum eleifend ultricies. Vestibulum efficitur tempor dolor."
 }
+
+export const Technologies = [
+    {
+        title: "javascript",
+        img: "/assets/JS.svg"
+    },
+    {
+        title: "HTML",
+        img: "/assets/HTML.svg"
+    },
+    {
+        title: "CSS",
+        img: "/assets/CSS.svg"
+    },
+    {
+        title: "TypeScript",
+        img: "/assets/Typescript.svg"
+    },
+    {
+        title: "React",
+        img: "/assets/react.svg"
+    },
+]
